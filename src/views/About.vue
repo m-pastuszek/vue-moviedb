8f227d1e6f2fb8103f1fc1cd681e4d4bad00b8ba
Marcin Pastuszek <m.pastuszek@moj-sklep.pl>
2021-06-04 12:25:05 +0200

    Prod build path change and About route update.

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,9 +4,19 @@
       O projekcie
     </v-card-title>
     <v-card-text>
+      <div class="body-1"></div>
       <div class="body-1">Projekt i wykonanie: <strong>Marcin Pastuszek</strong></div>
       <div class="body-1">Data realizacji: <strong>4 czerwca 2021</strong></div>
       <div class="body-1">Nr albumu: <strong>108599</strong></div>
+      <img class="mt-5" width="400px" src="logo_wsb_poznan.png"/>
+    </v-card-text>
+    <v-divider></v-divider>
+    <v-card-title>
+      API
+    </v-card-title>
+    <v-card-text>
+      <div class="body-1">W projekcie wykorzystano darmowe API <a href="https://www.themoviedb.org/" target="_blank">The Movie Database API</a>.</div>
+      <img class="mt-5" height="100px" src="tmdb logo.svg"/>
     </v-card-text>
   </v-card>
 </template>
