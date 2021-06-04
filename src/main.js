0e8c0de545768f419a4ac0c97f77ec7bc9c0d48f
Marcin Pastuszek <m.pastuszek@moj-sklep.pl>
2021-06-04 11:48:35 +0200

    Minor updates

--- src/main.js
+++ src/main.js
@@ -5,7 +5,6 @@ import store from './store'
 import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
-Vue.prototype.$appVersion = '1.0.4'
 Vue.prototype.$apiKey = '3e050bb165be726c2a0ad52a1e78b4c4'
 
 Vue.filter('truncate', function (text, stop, clamp) {
--- src/views/About.vue
+++ src/views/About.vue
@@ -5,7 +5,7 @@
     </v-card-title>
     <v-card-text>
       <div class="body-1">Projekt i wykonanie: <strong>Marcin Pastuszek</strong></div>
-      <div class="body-1">Data realizacji: <strong>2 czerwca 2021</strong></div>
+      <div class="body-1">Data realizacji: <strong>4 czerwca 2021</strong></div>
       <div class="body-1">Nr albumu: <strong>108599</strong></div>
     </v-card-text>
   </v-card>
